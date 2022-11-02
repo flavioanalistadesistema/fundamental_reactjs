42dda9e348ce9429eae1886d7b53c6a60ab61775
Flávio Santos <flavio.santos@pravaler.com.br>
2022-11-02 12:05:15 -0300

    feat: CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "../src/Post";
+import { Header } from "./components/Header";
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Flávio Santos"
         content="Desenvolvedor Senior"
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import style from './Header.modules.css'
+
+export function Header() {
+  return (
+    <header className={style.Header}>
+      <strong>igneti Fenite</strong>
+    </header>
+  );
+}
