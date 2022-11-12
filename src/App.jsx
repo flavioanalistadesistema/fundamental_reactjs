edce51c1e34b638f8c907ae74c1ea028efab10dc
Flávio Santos <flavio.santos@pravaler.com.br>
2022-11-12 10:11:48 -0300

    feat: propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,32 @@ import "./global.css";
 
 const post = [
   {
-      id: 1,
-      author: {
-          avatarUrl: 'https://github.com/flavioanalistadesistema.png',
-          name: 'Flavio Santos',
-          role: 'Programmer Tecnology'
-      },
-      content: [
-          {type: 'paragraph', content:'Fala galeraa 👋'},
-          {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-          {type: 'link', content:'flavio/santos'}
-      ],
-      publishedAt: '2022-11-12 08:00:00'
+    id: 1,
+    author: {
+        avatarUrl: 'https://github.com/flavioanalistadesistema.png',
+        name: 'Flavio Santos',
+        role: 'Programmer Tecnology'
+    },
+    content: [
+        {type: 'paragraph', content:'Fala galeraa 👋'},
+        {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content:'flavio/santos'}
+    ],
+    publishedAt: new Date('2022-11-12 08:00:00')
+  },
+  {
+    id: 2,
+    author: {
+        avatarUrl: 'https://github.com/thiagoaviscardi.png',
+        name: 'Thiago Vascardi',
+        role: 'Programmer Tecnology'
+    },
+    content: [
+        {type: 'paragraph', content:'Fala galeraa 👋'},
+        {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content:'thiago/varcardi'}
+    ],
+    publishedAt: new Date('2022-01-10 08:00:00')
   }
 ]
 
